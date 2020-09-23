5e0f1d3f70526b40d8dedc1c20788da93a576335
stsfang <steve5fang@163.com>
2020-09-23 20:02:48 +0800

    add tcpconnection test

---
+++ test/tcpconnection/test01.cc
@@ -0,0 +1,44 @@
+#include "chive/net/TcpConnection.h"
+#include "chive/net/EventLoop.h"
+#include "chive/net/InetAddress.h"
+#include "chive/net/TcpServer.h"
+#include "chive/base/clog/chiveLog.h"
+
+
+using namespace chive;
+using namespace chive::net;
+
+
+void onConnection(const TcpConnectionPtr& conn)
+{
+    if (conn->isConnected())
+    {
+        CHIVE_LOG_INFO("new connection %s from %s",
+                            conn->name().c_str(),
+                            conn->peerAddress().toIpPort().c_str());
+    }
+    else 
+    {
+        CHIVE_LOG_DEBUG("connection %s is down", conn->name().c_str());
+    }
+}
+
+void onMessage(const TcpConnectionPtr& conn, const char* data, ssize_t len)
+{
+    CHIVE_LOG_DEBUG("receive %d bytes from connection %s",
+                            len, conn->name().c_str());
+}
+
+int main()
+{
+    startLogPrint(NULL);
+    InetAddress listenAddr(9909);
+    EventLoop loop;
+
+    TcpServer server(&loop, listenAddr);
+    server.setConnectionCallback(onConnection):
+    server.setMessageCallback(onMessage);
+    server.start();
+
+    loop.loop();
+}
